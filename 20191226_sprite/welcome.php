c487994130169bcf930865f9246d7f6cd89fdcb7
seungheon314 <seungheon314@gmail.com>
2020-01-02 15:34:54 +0900

    today up

---
+++ 20191226_sprite/welcome.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>title</title>
+</head>
+<body>
+  <?
+	$name = $_GET["name"];
+	$email = $_GET["email"];
+	?>
+	
+	
+	이름:<?echo $name;?>님 반갑습니다.<br>
+	이메일:<?echo $email;?>
+</body>
+</html>
